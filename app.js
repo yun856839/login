9388e5873f190942d0248815c6154c5b764168dc
yun <n856839@gmail.com>
2020-11-18 15:00:34 +0800

    feat: add login check function

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const loginCheck = require('./loginCheck')
 const app = express()
 const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.post('/', (req, res) => {
+  // console.log('loginUser', req.body)
+  const userNow = loginCheck(req.body)
+  // console.log('Now User', userNow)
+  if (userNow) {
+    res.render('welcome', { userNow })
+  } else {
+    const wrong = 'Username 或 Password 錯誤 !!'
+    res.render('index', { wrong })
+  }
+})
+
 app.listen(port, () => {
-  console.log('server running')
+  console.log(`Express is listening on http://localhost:${port}`)
 })
